567b989b091dc53ee1db1e4539e0f602393d60fc
IlanRepiso <101400193+IlanRepiso@users.noreply.github.com>
2022-04-12 13:54:25 -0300

    Terminado

---
+++ Assets/Scenes/Text1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text1 : MonoBehaviour
+{
+    public int DiasTotales;
+    public int DiasLluvia;
+    public int CantDeUnidades;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DiasTotales < 5 || DiasLluvia < 0 || DiasLluvia > DiasTotales)
+        {
+            Debug.Log("Error 404. Syntax Error");
+        }
+        else
+        {
+            int DiasNoLluvia = DiasTotales - DiasLluvia;
+            int LitroCombustible = (DiasLluvia * 8) + (DiasNoLluvia * 6);
+            int PrecioCombustible = LitroCombustible * 130 * CantDeUnidades;
+            Debug.Log("Una flota de "+ CantDeUnidades +" unidades trabajando durante "+ DiasTotales +" días implicará un gasto de "+ PrecioCombustible +" pesos en concepto de combustible");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
